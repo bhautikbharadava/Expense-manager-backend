3300344dc058e79bd7a6fc1bc86568b7def667af
Bhautik Bharadava <bhautikbharadava@gmail.com>
2018-09-28 23:59:40 +0530

    Mongo added

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 // server.js
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const MongoClient = require("mongodb").MongoClient;
+
 const app = express();
 
 const port = 8000;
 
-require("./app/routes")(app, {});
-
 //converting to JSON from url encoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, () => {
-  console.log("We are live on " + port);
-});
+MongoClient.connect(
+  db.url,
+  (err, database) => {
+    if (err) return console.log(err);
+
+    // Make sure you add the database name and not the collection name
+    db = database.db("note-api");
+    require("./app/routes")(app, db);
+    app.listen(port, () => {
+      console.log("We are live on " + port);
+    });
+  }
+);
